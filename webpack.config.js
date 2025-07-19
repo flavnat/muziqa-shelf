523414502840edfe7bde58003f9e46cb6fd7e8fd
flavnat <natnael01ayele@gmail.com>
2025-07-20 00:16:39 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+                type: 'javascript/auto',
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new Dotenv(),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+};
